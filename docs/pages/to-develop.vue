38322428efbcaa9271d8f84a7e9c094b08183a46
Yash Shah <yashshah518@gmail.com>
2022-06-21 11:20:01 -0500

    v1 of library

---
+++ docs/pages/to-develop.vue
@@ -0,0 +1,129 @@
+<template lang="html">
+  <div class="basic-demo">
+    <h1>All Demo</h1>
+    <button type="button" name="button" @click="tags.push({ text: 'buh' })">add tag</button>
+    <button type="button" name="button" @click="tags.splice(1, 1)">delete tag</button>
+      <vue-tags-input
+        class="tags-input"
+        v-model="tag"
+        :tags.sync="tags"
+        :add-on-key="[188, 186, 13]"
+        :save-on-key="[188, 38, 13]"
+        :allow-edit-tags="true"
+        :separators="[',']"
+        :validation="validation"
+        :autocomplete-items="autocompleteItems"
+        @tags-changed="tagsChanged">
+      </vue-tags-input>
+      {{ tags }}
+  </div>
+</template>
+
+<script>
+import VueTagsInput from '../../vue-tags-input/vue-tags-input.vue';
+import TagInput from '../../vue-tags-input/tag-input.vue';
+
+export default {
+  name: 'ToDevelop',
+  components: {
+    VueTagsInput,
+    TagInput,
+  },
+  data() {
+    return {
+      tag: '',
+      tags: [{
+        classes: 'test',
+        text: 'uh',
+      }, {
+        text: 't5',
+      }],
+      validation: [{
+        type: 'min-length',
+        rule: '[0-9]',
+      }, {
+        type: 'only-numbers',
+        rule: '[0-9]',
+      }],
+      countries: [
+        'Germany', 'Holland', 'France', '09879', 't1', 't1',
+      ],
+      autocompleteItems: [],
+    };
+  },
+  methods: {
+    tagsChanged(tags) {
+      console.log('changed', tags);
+    },
+    tagClicked(obj) {
+      console.log(obj);
+    },
+    test(i) {
+      console.log(i);
+    },
+    foundDuplicate(tag) {
+      console.log(tag);
+    },
+    decideDeleting(obj) {
+      obj.deleteTag(true);
+    },
+    decideEdit(obj) {
+      obj.editTag(true);
+    },
+    decideAdding(obj) {
+      obj.addTag(true);
+    },
+    decideSaving(obj) {
+      obj.saveTag(true);
+    },
+    addInvalidOnPurpose(tag) {
+      this.tags.push(tag);
+    },
+    setAutocompleteItems() {
+      // if (this.tag.length === 0) return this.autocompleteItems = [];
+      this.autocompleteItems = this.countries
+        .filter(c => c.toLowerCase().indexOf(this.tag.toLowerCase()) !== -1)
+        .map(text => {
+          return { text, test: 'nlub' };
+        });
+    },
+    addTag() {
+      this.tags.push({ text: ('hu') });
+    },
+  },
+  watch: {
+    tag: 'setAutocompleteItems',
+  },
+  created() {
+    for (let i = 0; i < 10; i++) {
+      this.tags.push({
+        text: 'tag' + i,
+        [i]: 'test',
+      });
+    }
+  },
+  mounted() {
+    this.setAutocompleteItems();
+  },
+};
+</script>
+
+<style lang="scss">
+@import '~colors';
+
+.basic-demo .tag.duplicate {
+  background-color: #141f2d;
+}
+
+.basic-demo .tag.invalid.only-numbers {
+  background-color: $warn;
+}
+
+.basic-demo .new-tag-input.invalid.min-length {
+  color: $error;
+}
+
+.basic-demo .new-tag-input.invalid.only-numbers {
+  color: $warn;
+}
+</style>
---
+++ vue-tags-input/create-tags.js
@@ -0,0 +1,64 @@
+// helper functions
+
+const validateUserRules = (text, validation) => {
+  return validation.filter(val => {
+    // if the rule is a string, we convert it to RegExp
+    if (typeof val.rule === 'string') return !new RegExp(val.rule).test(text);
+
+    if (val.rule instanceof RegExp) return !val.rule.test(text);
+
+    // if we deal with a function, invoke it
+    const isFunction = {}.toString.call(val.rule) === '[object Function]';
+    if (isFunction) return val.rule(text);
+
+  }).map(val => val.type);
+};
+
+const createClasses = (text, tags, validation = [], checkFromInside = true) => {
+  // create css classes from the user validation array
+  const classes = validateUserRules(text, validation);
+
+  // check whether the tag is a duplicate or not
+  const duplicate = checkFromInside ? tags.filter(t => t.text === text).length > 1 :
+    tags.map(t => t.text).indexOf(text) !== -1;
+
+  // if it's a duplicate, push the class duplicate to the array
+  //if (duplicate) classes.push('duplicate');
+
+  // if we find no classes, the tag is valid → push the class valid
+  classes.length === 0 ? classes.push('valid') : classes.push('invalid');
+  return classes;
+};
+
+/**
+ * @description Create one tag out of a String or validate an existing one
+ * @property {helpers}
+ * @param {Object|String} tag A tag which should be validated | A String to create a tag
+ * @param {Array} tagsarray The tags array
+ * @param {Array} [validation=[]] The validation Array is optional (pass it if you use one)
+ * @returns {Object} The created (validated) tag
+ */
+const createTag = (tag, ...rest) => {
+  // if text is undefined, a string is passed. let's make a tag out of it
+  if (tag.text === undefined) tag = { text: tag };
+
+  // we better make a clone to not getting reference trouble
+  const t = JSON.parse(JSON.stringify(tag));
+
+  // create the validation classes
+  t.tiClasses = createClasses(t.text, ...rest);
+  return t;
+};
+
+/**
+ * @description Create multiple tags out of Strings or validate existing tags
+ * @property {helpers}
+ * @param {Array} tagsarray An Array containing tags or strings. See example below.
+ * @param {Array} [validation=[]] The validation Array is optional (pass it if you use one)
+ * @returns {Array} An array containing (validated) tags
+ * @example  &#47;* Example to call the function *&#47;
+   const validatedTags = createTags(['tag1Text', 'tag2Text'], [{ type: 'length', rule: /[0-9]/ }])
+ */
+const createTags = (tags, ...rest) => tags.map(t => createTag(t, tags, ...rest));
+
+export { createClasses, createTag, createTags };
